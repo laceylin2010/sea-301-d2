a4eb27dc332c4f55e585a39ca696bd181a879711
Edward Smith <sspohC@gmail.com>
2016-01-07 15:02:40 -0800

    Added Handlebar integration

--- class-04-templates/pair-assignment/lacey-edward/scripts/article.js
+++ class-04-templates/pair-assignment/lacey-edward/scripts/article.js
@@ -14,6 +14,11 @@ Article.prototype.toHtml = function() {
   //       - Get your template from the DOM.
   //       - Now "compile" your template with Handlebars.
 
+  var articleTemplate = $('#myArticles').html();
+  var compileTemplate = Handlebars.compile(articleTemplate);
+  var html = compileTemplate(this);
+
+
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced by key in the template.
@@ -22,6 +27,10 @@ Article.prototype.toHtml = function() {
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
   // TODO: Use the function that Handlebars gave you to return your filled-in html template for THIS article.
+
+
+    $('#app').append(html);
+
 };
 
 rawData.sort(function(a,b) {
