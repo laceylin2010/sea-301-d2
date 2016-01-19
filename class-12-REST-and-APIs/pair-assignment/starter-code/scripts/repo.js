77b5bbfd6cd04268ccf4c15fff174d705247bf8f
Lacey Vu <laceylin2010@gmail.com>
2016-01-19 12:06:57 -0800

    completed first to do

--- class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repo.js
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repo.js
@@ -4,9 +4,20 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    // TODO: How would you like to fetch your repos? Don't forget to call the callback.
+    // DONE: How would you like to fetch your repos? Don't forget to call the callback.
 
-  };
+
+//fetch repos with AJAX - first requirement
+//write a a success function to do something- 2nd requirement
+var qs = '?per_page=100&sort=updated');
+
+$.get('/github/users/laceylin2010/repos' + qs)
+.done(function(data, message, xhr){
+  console.log('my data - ' + data);
+  repos.all = data;
+  })
+  .done(callback);
+};
 
   // DONE: Model method that filters the full collection for repos with a particular attribute.
   // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
